3a63d86749e185518d10b844e4c3eaa89b3d3cd8
rizkimahendra17 <riskimhndr170@gmail.com>
2021-11-07 18:50:47 +0700

    finish migration Camp_Bnefit, Checkout

---
+++ database/migrations/2021_11_07_104626_create_camp_benefits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('camp_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+
+            // ini kita relasi kan dari tabel camps, kita buat foreignkey nya camps_id di table benefit,
+            //dan referensi nya id dari tabel camps
+            $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
