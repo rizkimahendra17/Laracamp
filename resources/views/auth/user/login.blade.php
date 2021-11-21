e436662ec4ede4c3272dab5ee8482ec863e1878d
rizkimahendra17 <riskimhndr170@gmail.com>
2021-11-21 21:46:16 +0700

    install and intgrate socialate

--- resources/views/auth/user/login.blade.php
+++ resources/views/auth/user/login.blade.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/main.css')}}" type="text/css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -19,11 +19,27 @@
 
 <body>
 
-   @include('components.navbar')
+    <section class="login-user">
+        <div class="left">
+            <img src="{{ asset('images/ill_login_new.png') }}" alt="">
+        </div>
+        <div class="right">
+            <img src="{{ asset('images/logo.png') }}" class="logo" alt="">
+            <h1 class="header-third">
+                Start Today
+            </h1>
+            <p class="subheader">
+                Because tomorrow become never
+            </p>
+            <p>
+                <a class="btn btn-border btn-google-login" href="{{ route('user.login.google') }}">
+                    <img src="{{ asset('images/ic_google.svg') }}" class="icon" alt=""> Sign In with Google
+                </a>
+            </p>
+            <img src="{{ asset('images/people.png') }}" class="people" alt="">
+        </div>
+    </section>
 
-
-
-    @yield('content')
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
